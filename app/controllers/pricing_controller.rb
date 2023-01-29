3068591dfc1f56a026c258f1491fa6d2f90bb092
joe953 <joemazloum953@gmail.com>
2023-01-29 21:23:35 +0200

    pricing and copyrighting web hosting

---
+++ app/controllers/pricing_controller.rb
@@ -0,0 +1,16 @@
+class PricingController < ApplicationController
+  def pricing_web
+  end
+
+  def pricing_mobile
+  end
+
+  def pricing_hosting
+  end
+
+  def pricing_marketing
+  end
+
+  def pricing_design
+  end
+end
