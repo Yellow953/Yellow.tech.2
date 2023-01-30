9ccbfbb4c5718ea22fd83b9a6aa423ae0ba9a5af
joe953 <joemazloum953@gmail.com>
2023-01-30 21:22:42 +0200

    copywriting

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,5 @@ Rails.application.routes.draw do
   get "marketing", to: "software#marketing", as: "marketing"
   get "hosting", to: "software#hosting", as: "hosting"
 
-  get "pricing/web", to: "pricing#pricing_web", as: "pricing_web"
-  get "pricing/mobile", to: "pricing#pricing_mobile", as: "pricing_mobile"
-  get "pricing/design", to: "pricing#pricing_design", as: "pricing_design"
-  get "pricing/marketing", to: "pricing#pricing_marketing", as: "pricing_marketing"
-  get "pricing/hosting", to: "pricing#pricing_hosting", as: "pricing_hosting"
-
   root "application#index"
 end
